366cfeac6f5fdab6c451df3179a8d1c378be8106
Jonathan Colegrove <Demetrius472@gmail.com>
2021-12-14 21:33:01 -0500

    Delete quintrix.training.com directory

--- quintrix.training.com/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module quintrix.training.com {
-	requires org.junit.jupiter.api;
-	requires junit;
-	requires org.testng;
-}
--- quintrix.training.com/src/quintrix/training/com/IEngine.java
+++
@@ -1,7 +0,0 @@
-package quintrix.training.com;
-
-public interface IEngine {
-	public int cylinderCount = 0;
-	
-	public void Start();
-}
