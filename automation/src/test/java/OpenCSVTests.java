7e919824bffefeaa1e4b120896a3c863e2cc931b
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-03 15:58:53 -0500

    added arrange act assert

--- automation/src/test/java/OpenCSVTests.java
+++ automation/src/test/java/OpenCSVTests.java
@@ -7,39 +7,67 @@ import com.opencsv.CSVReader;
 public class OpenCSVTests {
 
 	final String SOURCE = "src/test/resources/config.properties";
-	
+
 	public String ReadMe() throws Exception {
-	    Reader text = Files.newBufferedReader(Paths.get(
-	      ClassLoader.getSystemResource(SOURCE).toURI()));
-	    return text.toString();
+		Reader text = Files.newBufferedReader(Paths.get(
+				ClassLoader.getSystemResource(SOURCE).toURI()));
+		return text.toString();
 	}
 
 	//  A. ensure that you can get the 'browserType' value from the config file
 
 	@Test
 	public void getBrowserTypeValueFromConfigFile() {
-		
+
+		// Arrange
+		try {
+			System.out.println(ReadMe().contains("BrowserType"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Act
+
+
+		// Assert
+
+
 	}	
-	
-	
-	
-	
-	
-	
-//  B. Write a test which reads the basses.csv file into a HashMap of Makes & Models. Ignore string count.
+
+
+
+
+
+
+	//  B. Write a test which reads the basses.csv file into a HashMap of Makes & Models. Ignore string count.
 
 	@Test
 	public void canReadBassesFileIntoHashmapOfMakesAndModelsIgnoringStringCount() {
-		
+
+		// Arrange
+
+
+		// Act
+
+
+		// Assert
+
 	}
 
-	
-	
-//  C. Hydrate an object with CSV file data
-//  reads the basses.csv file into a Basses Object
-	
+
+
+	//  C. Hydrate an object with CSV file data
+	//  reads the basses.csv file into a Basses Object
+
 	@Test
 	public void readsBassesFileIntoBassesObject() {
+		// Arrange
+
+
+		// Act
+
+
+		// Assert
 
 	}
 }
