3b199655f208bae973f113cd9ba73d50e92edc54
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-03 13:37:42 -0500

    removed previous Test.java, renamed Trial.java

--- automation/src/test/java/OpenCSVTests.java
+++ automation/src/test/java/OpenCSVTests.java
@@ -1,173 +1,45 @@
-//import static org.testng.Assert.assertNotNull;
-
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.Reader;
-
-import org.testng.Assert;
-//import org.testng.annotations.DataProvider;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.testng.annotations.Test;
+import com.opencsv.CSVReader;
 
-public class OpenCSVTests { 
+public class OpenCSVTests {
 
 	final String SOURCE = "src/test/resources/config.properties";
 	
-	
-//  A. ensure that you can get the 'browserType' value from the config file
-  
-  @Test
-  public void getBrowserTypeValueFromConfigFile() {
-	  
-	//	Arrange
-//	  * make file reader object
-	  
-	  Reader reader = null;
-	try {
-		reader = new FileReader(SOURCE);
-	} catch (FileNotFoundException e1) {
-		e1.printStackTrace();
+	public String ReadMe() throws Exception {
+	    Reader text = Files.newBufferedReader(Paths.get(
+	      ClassLoader.getSystemResource(SOURCE).toURI()));
+	    return text.toString();
 	}
 
-		int data = 0;
-		try {
-			data = reader.read();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}{
+	//  A. ensure that you can get the 'browserType' value from the config file
 
-			while(data != -1){
-				try {
-					data = reader.read();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			try {
-				reader.close();
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		}
-	 
-	//	Act
-//	  * find by ID, etc
-//		String found = reading.read(#LC2 > span.innerHtml);
-//		<td id="LC2" class="blob-code blob-code-inner js-file-line"><span class="pl-k">BrowserType</span>=chrome</td>
+	@Test
+	public void getBrowserTypeValueFromConfigFile() {
+		
+	}	
 	
-		//	Assert
-//		Assert.assertEquals(found, "BrowserType");
-  }
-  
+	
+	
+	
+	
+	
 //  B. Write a test which reads the basses.csv file into a HashMap of Makes & Models. Ignore string count.
-  
-  @Test
-  public void canReadBassesFileIntoHashmapOfMakesAndModelsIgnoringStringCount() {
-		//	Arrange
-//	  		* make file reader object
-	  		
-	  		Reader reader = null;
-			try {
-				reader = new FileReader(SOURCE);
-			} catch (FileNotFoundException e1) {
-				e1.printStackTrace();
-			}
 
-			int data = 0;
-			try {
-				data = reader.read();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}{
+	@Test
+	public void canReadBassesFileIntoHashmapOfMakesAndModelsIgnoringStringCount() {
+		
+	}
 
-				while(data != -1){
-					try {
-						data = reader.read();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-
-				try {
-					reader.close();
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-			}
-		//	Act
-//			  * read into Hashmap of Makes & Models, ignore string count
-		//	Assert
-//			  Assert.assertEquals(0, 0)
-  }
-  
+	
+	
 //  C. Hydrate an object with CSV file data
 //  reads the basses.csv file into a Basses Object
-  
-  @Test
-  public void readsBassesFileIntoBassesObject() {
-		//	Arrange
-	  
-	  Reader reader = null;
-	try {
-		reader = new FileReader(SOURCE);
-	} catch (FileNotFoundException e3) {
-		e3.printStackTrace();
+	
+	@Test
+	public void readsBassesFileIntoBassesObject() {
+
 	}
-
-		int data = 0;
-		try {
-			data = reader.read();
-		} catch (IOException e3) {
-			e3.printStackTrace();
-		}{
-
-			while(data != -1){
-				try {
-					data = reader.read();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			try {
-				reader.close();
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		}
-	  
-	  
-	  
-		//	Act
-//		  read basses.csv into Basses object
-//	  * make file writer object
-	  
-	  OutputStream outputStream = null;
-		try {
-			outputStream = new BufferedOutputStream(new FileOutputStream(SOURCE));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			outputStream.write(data);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				outputStream.flush();
-				outputStream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		
-		//	Assert
-//	  assert not null?  assert Basses object written?
-		Assert.assertNotNull(outputStream);
-  }
 }
