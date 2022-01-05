74d58e07b3560d397b60e3d231b2c9aa4d229aa9
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-05 06:43:01 -0500

    CSS Selector hw assignment

---
+++ automation/src/test/java/selectors/selectors.java
@@ -0,0 +1,35 @@
+package selectors;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class selectors {
+	
+	WebDriver driver = new ChromeDriver();
+
+	@FindBy(how=How.ID, using="userName")
+	private WebElement TextBox;
+	
+	@FindBy(how=How.CLASS_NAME, using="btn btn-primary")
+	private WebElement Button;
+	
+	@FindBy(how=How.XPATH, using="Footer/Span")
+	private WebElement FooterSpan;
+	
+	@FindBy(how=How.CSS, using="#nav > li")
+	private WebElement List;
+	
+	@FindBy(how=How.CSS, using="#oldSelectMenu > option[value='4']")
+	private WebElement Radio;
+	
+	// JAVASCRIPT to get element with ID "sliderValue":    document.getElementById("sliderValue");
+	@FindBy(how=How.ID, using="sliderValue")
+	private WebElement Slider;
+	
+	// JAVASCRIPT to set element with ID "userName" to DennisTaylor:	document.getElementById("userName").value = "DennisTaylor";
+	@FindBy(how=How.ID, using="userName")
+	private WebElement userName;
+}
