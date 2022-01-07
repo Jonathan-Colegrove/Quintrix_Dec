5b6d901f4c87921466eb5d73ecf2138619fa505b
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-06 19:50:19 -0500

    Added chrome driver to file, created Jan 5 assignment

---
+++ automation/src/test/java/January5/DatePickerPage.java
@@ -0,0 +1,45 @@
+package January5;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import framework.PageObjectBase;
+
+public class DatePickerPage extends PageObjectBase {
+
+	private final String URL = "https://demoqa.com/date-picker";
+	
+	protected DatePickerPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(how=How.ID, using="datePickerMonthYearInput")
+	private WebElement DatePicker;
+	
+	@FindBy(how=How.ID, using="dateAndTimePickerInput")
+	private WebElement DateAndTimePicker;
+	
+	public void navigate() {
+		driver.navigate().to(URL);
+	}
+	
+	public void setValue(String value) {
+		DatePicker.sendKeys(value);
+	}
+	
+	public String getText() {
+		String text = DatePicker.getAttribute("value");
+		return text;
+	}
+	
+	public void setTimeValue(String value) {
+		DateAndTimePicker.sendKeys(value);
+	}
+	
+	public String getTimeText() {
+		String text = DateAndTimePicker.getAttribute("value");
+		return text;
+	}
+}
