43b0311149c675740a0406f72b0d6addad25ec2e
Jonathan Colegrove <Demetrius472@gmail.com>
2022-02-03 20:17:30 -0500

    set up Chrome Driver

---
+++ automation.review/src/test/java/WebDriverTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class WebDriverTest {
+	ChromeDriver driver;
+
+	@Test
+	public void canLaunchChromeDriver() {
+		//arrange
+
+		//act
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jonathan\\Desktop\\Quintrix\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+
+		//assert
+		Assert.assertNotNull(driver, "Chrome Driver is null");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		if(this.driver != null) {
+			this.driver.quit();
+		}
+	}
+
+}
